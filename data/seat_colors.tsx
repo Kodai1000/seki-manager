7c00e45cdf8ba6035ce40537fb180a172dad6c17
Kodai.T <fvbcj53523@yahoo.co.jp>
2026-09-10 01:15:16 +0900

    Refine seat colors

--- data/seat_colors.tsx
+++ data/seat_colors.tsx
@@ -1,18 +1,38 @@
 export const seat_colors = [
     {
-        name : "青",
-        color : "blue"
+        name: "青",
+        color: "#67e8f9" // bg-cyan-300 相当
     },
     {
-        name : "赤",
-        color : "red"
+        name: "赤",
+        color: "#fda4af" // bg-rose-300 相当
     },
     {
-        name : "緑",
-        color : "green"
+        name: "緑",
+        color: "#bef264" // bg-lime-300 相当
     },
     {
-        name : "オレンジ",
-        color: "orange"
-    }
+        name: "紫",
+        color: "#d8b4fe" // bg-purple-300 相当
+    },
+    {
+        name: "ピンク",
+        color: "#f472b6" // bg-pink-300 相当（少し調整）
+    },
+    {
+        name: "黄緑",
+        color: "#6ee7b7" // bg-emerald-300 相当
+    },
+    {
+        name: "インディゴ",
+        color: "#a5b4fc" // bg-indigo-300 相当
+    },
+    {
+        name: "黄",
+        color: "#fde047" // bg-yellow-300 相当
+    },
+    {
+        name: "グレー",
+        color: "#cbd5e1" // bg-slate-300 相当
+    },
 ]
